ed33ccba342c4bcbc934fbef25a1cc71ffb34d0d
Ayushi Pawar <aayushipawar2004@gmail.com>
2026-04-18 12:25:44 +0530

    Add files via upload

---
+++ 12. Remove Consecutive Duplicates from Sorted Linked List.py
@@ -0,0 +1,62 @@
+#12. Remove Consecutive Duplicates from Sorted Linked List
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+
+def deleteDuplicates(head):
+    # Write your code here
+    current = head
+    
+    while current and current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
+
+if __name__ == '__main__':
+    head_count = int(input().strip())
+
+    head = SinglyLinkedList()
+
+    for _ in range(head_count):
+        head_item = int(input().strip())
+        head.insert_node(head_item)
+
+    result = deleteDuplicates(head.head)
+
+    print_singly_linked_list(result, '\n')
+    print()
